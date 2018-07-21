825beb023cc8ef0a4345c82e876ceb2340dcd048
Jocelyn Jeffrey <jocelynjeffrey@gmail.com>
2018-07-21 13:06:15 -0400

    working on motion route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/motion', function(req, res, next) {
+  res.render('index', { title: 'MOOOOOTION' });
+});
+
 module.exports = router;
