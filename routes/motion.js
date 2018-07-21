ccd43cff48af3321b952da7dc6c953cce4136246
Jocelyn Jeffrey <jocelynjeffrey@gmail.com>
2018-07-21 19:40:23 -0400

    updates users to motion

---
+++ routes/motion.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET motion listing. ORIGINAL */
+// router.get('/', function(req, res, next) {
+//   res.send('motion');
+// });
+
+// version 2
+// router.post('/motion', function(req, res) {
+//   var msg = req.body.msg
+//   console.log(msg);
+//   alert(msg); // for some reason this keeps the loop open
+//   res.send("hey girl");
+// });
+
+router.get('/', function(req, res) {
+  var msg = req.body.msg
+  console.log(msg);
+  alert(msg); // for some reason this keeps the loop open
+  res.send('index', { title: 'Fortune Teller' });
+});
+
+module.exports = router;
