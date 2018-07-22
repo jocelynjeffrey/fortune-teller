0035c361b42fa34ed6bb197b4b98b27d2b0be51d
Jocelyn Jeffrey <jocelynjeffrey@gmail.com>
2018-07-22 11:18:06 -0400

    routes again

--- routes/motion.js
+++ routes/motion.js
@@ -2,29 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET motion listing. ORIGINAL */
-// router.get('/', function(req, res, next) {
-//   res.send('motion');
-// });
-
-router.post('/motion', (req, res) => {
-  var msg = req.body.msg;
-  console.log(msg);
-  res.send(msg);
+router.get('/', function(req, res, next) {
+  res.send('motion');
 });
 
 // version 2
-//router.post('/motion', function(req, res) {
-//   var msg = req.body.msg
-//   console.log(msg);
-//   alert(msg); // for some reason this keeps the loop open
-//   res.send("hey girl");
-// });
-
-//router.post('/motion', function(req, res) {
-//  var msg = req.body.msg
-//  console.log(msg);
-//  console.log('response is', res);
-//  alert('test');
-//});
+router.post('/motion', function(req, res) {
+  var msg = req.body.msg
+  console.log(msg);
+  res.send('motion 2');
+});
 
 module.exports = router;
