6f0fdfdd43100aa4b3cf33b166ab5ba0828c6247
Jocelyn Jeffrey <jocelynjeffrey@gmail.com>
2018-07-22 12:42:51 -0400

    added json to response

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use('/motion', motionRouter);
 
 app.post('/motion', (req, res) => {
   var msg = req.body.msg;
-  console.log('res from app.js', res.sendStatus(status));
-  res.send(res.sendStatus(status));
+  // console.log('res from app.js', res.sendStatus(200));
+  res.send(res.json({ motion: true }));
 });
 
 // catch 404 and forward to error handler
