776c70483d26ef88ebf277ff5b318c0c57d516d6
Jocelyn Jeffrey <jocelynjeffrey@gmail.com>
2018-07-22 12:34:40 -0400

    code

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use('/motion', motionRouter);
 
 app.post('/motion', (req, res) => {
   var msg = req.body.msg;
-  console.log('res from app.js', res.status());
-  res.send(res.status());
+  console.log('res from app.js', res.statusCode);
+  res.send(res.statusCode);
 });
 
 // catch 404 and forward to error handler
