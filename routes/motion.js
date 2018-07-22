ed4485f55ca720829d31c8bdc1d11115cc04b387
Jocelyn Jeffrey <jocelynjeffrey@gmail.com>
2018-07-22 13:54:09 -0400

    removed extra value

--- routes/motion.js
+++ routes/motion.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET motion listing. */
-router.get('/motion', function(req, res) {
+router.get('/', function(req, res) {
   console.log('REQUEST IS', req)
   res.send('motion from router');
 });
