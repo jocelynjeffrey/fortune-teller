297c5d9dc9bae76af3009e9dceb08ff5f922361c
Jocelyn Jeffrey <jocelynjeffrey@gmail.com>
2018-07-22 12:31:20 -0400

    more

--- app.js
+++ app.js
@@ -24,8 +24,8 @@ app.use('/motion', motionRouter);
 
 app.post('/motion', (req, res) => {
   var msg = req.body.msg;
-  console.log('msg from app.js', msg);
-  console.log('res from app.js', res);
+  console.log('msg from app.js', msg.status);
+  console.log('res from app.js', res.status);
   res.send(msg);
 });
 
