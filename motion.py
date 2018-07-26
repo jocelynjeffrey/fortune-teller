c45846de586fb23b6e001393b5d41612c27c639c
Jocelyn Jeffrey <jocelynjeffrey@gmail.com>
2018-07-26 23:04:52 +0000

    adding motion script!

---
+++ motion.py
@@ -0,0 +1,32 @@
+import json
+
+# import GPIO library
+import RPi.GPIO as GPIO
+
+# import time library
+import time
+
+#set GPIO pin numbering
+GPIO.setmode(GPIO.BOARD)
+#associate pin 7 to pir
+pir =7
+#set pin as GPIO in
+GPIO.setup(pir, GPIO.IN)
+
+#import requests library
+import requests
+
+#set api endpoint
+url = "http://localhost:3000/motion"
+data = {'msg': 'PI'}
+
+
+print ("Waiting for sensor to settle")
+time.sleep(2)                                     #Waiting 2 seconds for the sensor to initiate
+print ("Detecting...")
+while True:
+   if GPIO.input(pir):                            #Check whether pir is HIGH
+      print ("Motion Detected!")
+      r = requests.post(url, json=data)
+      time.sleep(1)                               #D1- Delay to avoid multiple detection
+   time.sleep(0.1)                                #While loop delay should be less than detection(hardware) delay
