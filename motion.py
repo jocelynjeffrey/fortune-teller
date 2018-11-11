c33e135107cbc8a15870f1dff5eaef36f5cf443b
Jocelyn Jeffrey <jocelynjeffrey@gmail.com>
2018-11-11 16:58:52 -0500

    wip

--- motion.py
+++ motion.py
@@ -26,9 +26,9 @@ while True:
    if GPIO.input(pir):                            #Check whether pir is HIGH
       print ("Motion Detected!")
       requests.post(url, json={'msg': 1})
-      time.sleep(1)                               #D1- Delay to avoid multiple detection
+      time.sleep(2)                               #D1- Delay to avoid multiple detection
    else:
       print ("waiting for motion")
       requests.post(url, json={'msg': 0})
-      time.sleep(1)
+      time.sleep(2)
 #   time.sleep(0.1)                                #While loop delay should be less than detection(hard$
