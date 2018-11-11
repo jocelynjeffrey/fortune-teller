1d48aee5473c533983bb3f7de32ee76fbdc2bfca
Jocelyn Jeffrey <jocelynjeffrey@gmail.com>
2018-11-11 16:52:33 -0500

    added back error handlers

--- app.js
+++ app.js
@@ -30,20 +30,20 @@ app.get('/fortune', (req, res) => {
   isMotion ? res.send('MOTION') : res.send('detecting...');
 });
 
-// // // catch 404 and forward to error handler
-// app.use((err, req, res, next) => {
-//   next(createError(404));
-// });
+// // catch 404 and forward to error handler
+app.use((err, req, res, next) => {
+  next(createError(404));
+});
 
-// // error handler
-// app.use((err, req, res, next) => {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
