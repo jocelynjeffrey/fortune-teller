4f38f3936f59bc214475dfce68d9622c83177817
Jocelyn Jeffrey <jocelynjeffrey@gmail.com>
2018-11-11 16:41:47 -0500

    wip

--- app.js
+++ app.js
@@ -31,12 +31,12 @@ app.get('/fortune', (req, res) => {
 });
 
 // // catch 404 and forward to error handler
-// app.use((req, res, next) => {
-//   next(createError(404));
-// });
+app.use((err, req, res, next) => {
+  next(createError(404));
+});
 
 // error handler
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
